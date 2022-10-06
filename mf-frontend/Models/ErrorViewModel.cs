c04f4d27aed304933bd37b1381c484b5bd682709
Ardhiyan Pratama <ardhiyanp@gmail.com>
2022-10-06 21:34:13 +0700

    first init

---
+++ mf-frontend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mf_frontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
