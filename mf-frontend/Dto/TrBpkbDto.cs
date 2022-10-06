9dccf74fb4bc2bef76186dee6a470a9ff96a075a
Ardhiyan Pratama <ardhiyanp@gmail.com>
2022-10-06 21:57:11 +0700

    added html

---
+++ mf-frontend/Dto/TrBpkbDto.cs
@@ -0,0 +1,15 @@
+﻿namespace mf_frontend.Dto
+{
+    public class TrBpkbDto
+    {
+        public string AgreementNumber { get; set; } = null!;
+        public string? BpkbNo { get; set; }
+        public string? BranchId { get; set; }
+        public DateTime? BpkbDate { get; set; }
+        public string? FakturNo { get; set; }
+        public DateTime? FakturDate { get; set; }
+        public string? PoliceNo { get; set; }
+        public DateTime? BpkbDateIn { get; set; }
+        public string? LocationId { get; set; }
+    }
+}
